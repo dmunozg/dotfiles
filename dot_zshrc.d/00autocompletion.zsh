c8daf081291eb8f5d7eded70c5e0ee2a70504904
dmunozg <dmunoz@bioscience.cl>
2024-08-08 10:42:59 -0400

    Add .zshrc.d/00autocompletion.zsh

---
+++ dot_zshrc.d/00autocompletion.zsh
@@ -0,0 +1,10 @@
+# Autocompletion
+autoload -Uz compinit
+typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
+if [ $(date +'%j') != $updated_at ]; then
+    compinit -i
+else
+    compinit -C -i
+fi
+
+zmodload -i zsh/complist
