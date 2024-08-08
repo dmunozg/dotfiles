62f7e1a9f012cfb2f02ab8e8697a0acafdd78cbd
dmunozg <dmunoz@bioscience.cl>
2024-08-08 10:44:01 -0400

    Add .zshrc.d/history.zsh Add .zshrc.d/kitty.sh Add .zshrc.d/path.zsh

---
+++ dot_zshrc.d/kitty.sh
@@ -0,0 +1,2 @@
+# Kitty over ssh
+[[ "$TERM" == "xterm-kitty" ]] && alias ssh="kitty +kitten ssh"
---
+++ dot_zshrc.d/path.zsh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Cargo
+path=($HOME'/.cargo/bin' $path)
+
+# pip
+path=($HOME'/.local/bin' $path)
+
+# Propagar
+export PATH
